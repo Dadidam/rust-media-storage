3e979cb2c86d06355d43b76d2e481664041cd4ae
Ilia Vorontcov <joker.pnz@gmail.com>
2025-01-02 19:02:31 -0500

    add some weird stuff (e.g. unlabeled fields)

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ enum Media {
     Book { title: String, author: String },
     Movie { title: String, director: String },
     Audiobook { title: String },
+    Podcast(u32), // <--- using unlabeled field here
+    Placeholder,
 }
 
 impl Media {
@@ -18,7 +20,28 @@ impl Media {
             Media::Audiobook { title } => {
                 format!("Audiobook: {}", title)
             }
+            Media::Podcast(episode_number) => {
+                format!("Podcast: {}", episode_number)
+            }
+            Media::Placeholder => {
+                format!("Just a Placeholder")
+            }
         }
+    }
+}
+
+#[derive(Debug)]
+struct Catalog {
+    items: Vec<Media>,
+}
+
+impl Catalog {
+    fn new() -> Self {
+        Catalog { items: vec![] }
+    }
+
+    fn add(&mut self, media: Media) {
+        self.items.push(media)
     }
 }
 
@@ -41,7 +64,23 @@ fn main() {
         title: String::from("Bad audiobook"),
     };
 
+    let podcast = Media::Podcast(777);
+
+    let placeholder = Media::Placeholder;
+
     println!("{}", book.description());
     println!("{}", good_movie.description());
     println!("{}", bad_audiobook.description());
+    println!("{}", podcast.description());
+    println!("{}", placeholder.description());
+
+    let mut catalog = Catalog::new();
+
+    catalog.add(book);
+    catalog.add(good_movie);
+    catalog.add(bad_audiobook);
+    catalog.add(podcast);
+    catalog.add(placeholder);
+
+    println!("{:#?}", catalog);
 }
